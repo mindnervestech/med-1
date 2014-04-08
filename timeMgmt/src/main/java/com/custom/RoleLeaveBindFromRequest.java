e611e9bfc374b71aa222062853af666a8abf9017
pravinerande <pberande1@gmail.com>
2014-04-08 11:59:05 +0530

    missing files

---
+++ timeMgmt/src/main/java/com/custom/RoleLeaveBindFromRequest.java
@@ -0,0 +1,19 @@
+package com.custom;
+
+import java.util.List;
+
+import models.RoleLeave;
+
+public class RoleLeaveBindFromRequest {
+	
+	List<RoleLeave>roleLeaves;
+
+	public List<RoleLeave> getRoleLeaves() {
+		return roleLeaves;
+	}
+
+	public void setRoleLeaves(List<RoleLeave> roleLeaves) {
+		this.roleLeaves = roleLeaves;
+	}
+
+}
